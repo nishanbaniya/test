cb5410defd880cfd9e7cb1b58e04f99282f9792f
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 20:19:48 +0545

    commit2

---
+++ calc.js
@@ -0,0 +1,16 @@
+var a=Number(prompt("Enter first number:"));
+var o=prompt("Enter the Operator");
+var b=Number(prompt("Enter second number:"));
+console.log("hey")
+// if(o == "+") {
+//   alert(a+b);
+// }
+// else if(o == "-") {
+//     alert(a-b);
+//   }
+//  else if(o == "*") {
+//     alert(a*b);
+//   }
+//  else if(o == "/") {
+//     alert(a/b);
+//   }
---
+++ finalPrice.js
@@ -0,0 +1,9 @@
+
+price=Number(prompt("What is the original price?"));
+discount=Number(prompt("What is the discount percentage?")/100);
+discountedPrice=price*discount;
+finalPrice=price-discountedPrice;
+alert("Discounted price: " + discountedPrice);
+console.log("Discounted price: ", discountedPrice);
+alert("Final Price: " + finalPrice);
+console.log("Final Price: ", finalPrice);
