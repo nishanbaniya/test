27245c72984c66d12721960a634a99d4645a0d62
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 23:46:49 +0545

    ary

--- function.js
+++ function.js
@@ -23,14 +23,25 @@
   
 
 //making try.js calculator using function
-function calculateFood(food, tipPer){
-    //food=food
-    const tipPercentage= tipPer/100;
-    const tipAmount= food * tipPercentage;
-    const total = Sum(food, tipAmount ) ;
-    return total;
-    }
-    console.log(calculateFood(100, 20));
+// function calculateFood(food, tipPer){
+//     //food=food
+//     const tipPercentage= tipPer/100;
+//     const tipAmount= food * tipPercentage;
+//     const total = Sum(food, tipAmount ) ;
+//     return total;
+//     }
+//     console.log(calculateFood(100, 20));
+function sum(a, b) {
+    return a + b
+}
+
+function calculateFoodTotal(food, tip) {
+    tipPercentage = tip / 100
+    tipAmount = food * tipPercentage
+    total = sum(food, tipAmount)      //above function "sum" is used here.
+    return total
+}
+console.log(calculateFoodTotal(200, 20))
 
 //arrouw function with explicit return
 // const myFunction =(a,b) => {
