35bb031ae7760b6e654eb1d3aa9108393d2def51
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 21:09:23 +0545

    weather

---
+++ weather.js
@@ -0,0 +1,14 @@
+weather=prompt("How is the weather?")
+
+if(weather =="rainy"){
+    console.log("Take an Umbrella");
+    alert("Take an Umbrella");
+}
+else if(weather =="sunny"){
+    console.log("Wear a Sunglasses");
+    alert("Wear a Sunglasses");
+}
+else{
+    console.log("Stay in house");
+    alert("Stay in house");
+}
