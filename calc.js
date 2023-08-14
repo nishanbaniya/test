bf4b2c02d33475399647d05c024361bda4a6df57
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 21:45:49 +0545

    math

--- calc.js
+++ calc.js
@@ -18,3 +18,21 @@ else if(o == "/") {
   } 
   console.log(c);
   alert("The answer is: " + c)
+
+
+//   math methods
+// a=10.11;                         
+// b=Math.floor(a);                //point paxadi jati vayeni point agadi ko matra lekhxa. i.e Rounds down
+// console.log(b);
+
+// c=10.11;
+// d=Math.ceil(c);                   //point paxadi jati vayeni point agadi ko +1  lekhxa.  i.e. Rounds Up
+// console.log(d);
+
+//Math.random()                 // Used for generating random number .this will generating random numbers between o and 1.
+//Math.random()*100             //This will generate random numbers between 0 and 100.
+
+
+//Rock Seissor Paper
+//Math.floor(Math.random()*3);     // This will generate only integers numbers between 0 and 2.
+
