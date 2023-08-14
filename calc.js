b89be424668eee5e595ff17308bebfe3553e39ad
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 20:26:10 +0545

    afterCalc

--- calc.js
+++ calc.js
@@ -1,16 +1,17 @@
 var a=Number(prompt("Enter first number:"));
 var o=prompt("Enter the Operator");
 var b=Number(prompt("Enter second number:"));
-console.log("hey")
-// if(o == "+") {
-//   alert(a+b);
-// }
-// else if(o == "-") {
-//     alert(a-b);
-//   }
-//  else if(o == "*") {
-//     alert(a*b);
-//   }
-//  else if(o == "/") {
-//     alert(a/b);
-//   }
+if(o == "+") {
+  c=a+b;
+}
+else if(o == "-") {
+    c=a-b;
+  }
+else if(o == "*") {
+    c=a*b;
+  }
+else if(o == "/") {
+    c=a/b;
+  }
+  console.log(c);
+  alert("The answer is: " + c)
