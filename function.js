91b7b62242a9fd01181d5387acca431134ebd058
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 23:19:33 +0545

    function

---
+++ function.js
@@ -0,0 +1,44 @@
+//function without argument
+// function myName(){
+//   console.log("My Name is Nishan");
+// }
+// myName();
+
+
+//function with 1 arguement
+// function myName2(name){
+//     sth=`My Name is ${name} `    //is equivalent to sth="My name is " + name ,
+//     console.log(sth);
+//   }
+//   myName2("Nishan Baniya");
+
+//making sum
+// function sum(a,b) {
+//     return a+b;
+    
+// }
+// c=sum(1,2);
+// console.log(c);
+
+  
+
+//making try.js calculator using function
+function calculateFood(food, tipPer){
+    //food=food
+    const tipPercentage= tipPer/100;
+    const tipAmount= food * tipPercentage;
+    const total = Sum(food, tipAmount ) ;
+    return total;
+    }
+    console.log(calculateFood(100, 20));
+
+//arrouw function with explicit return
+// const myFunction =(a,b) => {
+//     return a+b
+// }
+
+// //arrouw function with implicit return
+// const myFunction2 =(a,b) => a+b
+// console.log(myFunction2(4,3) )
+
+
