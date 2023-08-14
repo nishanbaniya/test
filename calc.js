3dd85de19b81451cfb395165bac03d287c21f8f9
Nishan Baniya <nishanbaniya88@gmail.com>
2023-08-14 21:23:37 +0545

    after exponential

--- calc.js
+++ calc.js
@@ -13,5 +13,8 @@ else if(o == "*") {
 else if(o == "/") {
     c=a/b;
   }
+  else if(o == "**") {            //power i.e. a ko power ma b. called exponential
+    c=a**b;
+  } 
   console.log(c);
   alert("The answer is: " + c)
